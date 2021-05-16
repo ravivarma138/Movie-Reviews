953d76c6b6496549ff4cf00cb6a11b87293dd8ca
Rohit Peela <jb199815@gmail.com>
2021-05-16 16:13:42 +0530

    Loader Added

--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -4,24 +4,31 @@ import YouTube from 'react-youtube';
 import movieTrailer from 'movie-trailer';
 import "./Row.css";
 import { Scrollbars } from 'react-custom-scrollbars';
+import { SunspotLoader,BoxesLoader,ScatterBoxLoader  } from "react-awesome-loaders";
 import Skeleton from 'react-loading-skeleton';
-const base_url = "https://image.tmdb.org/t/p/original"
+const base_url = "https://image.tmdb.org/t/p/original";
+
 
 function Row({ title, fetchUrl, isLargeRow }) {
     const [movies, setMovies] = useState([]);
     const [trailerUrl, setTrailerUrl] = useState("");
+    const [loading, isLoading] = useState(true);
 
     useEffect(() => {
 
         async function fetchData() {
-
-            const request = await axios.get(fetchUrl);
-            console.log(request.data);
-            setMovies(request.data.results);
-            return request;
+            await axios.get(fetchUrl).then(result => {
+                console.log(result.data);
+                setMovies(result.data.results);
+                let timer1 = setTimeout(() => isLoading(false), 5 * 1000);
+                // isLoading(false);
+                // clearTimeout(timer1);
+            })
         }
         fetchData();
     }, [fetchUrl]);
+
+
 
     const opts = {
         height: "390",
@@ -44,24 +51,33 @@ function Row({ title, fetchUrl, isLargeRow }) {
         }
     }
 
-    return (
+    return loading ? (
+        <div className="loader">
+            <BoxesLoader
+        boxColor={"#6366F1"}
+        style={{ marginBottom: "20px" }}
+        desktopSize={"128px"}
+        mobileSize={"80px"}
+      />
+        </div>
+    ) : (
+
         <div className="row">
-            <h2>{title || <Skeleton duration={5}/>}</h2>
+            <h2>{title || <Skeleton duration={5} />}</h2>
             <div className="row_posters">
                 {movies.map(movie => {
                     return <><img
                         key={movie.id}
                         // onClick={() => handleClick(movie)}
                         className={`row_poster ${isLargeRow && "row_posterLarge"}`}
-                        src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}` || <Skeleton width={100} duration={5}/>}
+                        src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}` || <Skeleton width={100} duration={5} />}
                         alt={movie.name} /></>
                 })}
             </div>
-            {/* <div style={{ padding: "40px" }}>
-                {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
-            </div> */}
         </div>
-    );
+
+    )
+
 }
 
 export default Row;
