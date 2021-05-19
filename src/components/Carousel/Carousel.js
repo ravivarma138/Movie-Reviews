0309b095c9e2db1f25c71c7df826282884c2cbbb
Rohit Peela <jb199815@gmail.com>
2021-05-19 13:24:48 +0530

    Carousel fix

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -36,7 +36,7 @@ const Carousel = ({ id, media_type }) => {
 
   const fetchCredits = async () => {
     const { data } = await axios.get(
-      `https://api.themoviedb.org/3/${media_type}/${id}/credits?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+      `https://api.themoviedb.org/3/${media_type}/${id}/credits?api_key=${requests.api}&language=en-US`
     );
     setCredits(data.cast);
   };
